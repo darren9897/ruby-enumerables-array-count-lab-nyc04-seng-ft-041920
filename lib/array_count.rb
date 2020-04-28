678e651be96715a71ddd6864efdc3ae5b46dab37
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-28 19:44:01 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,11 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
  count = 0
- 
+ alpha = "abcdefghijklmnopqrstuvwxyz"
  
   array.count do |num|
     
-    if num == num.include?("")
+    if num[0].include(alpha)
       count += 1
       
     end
